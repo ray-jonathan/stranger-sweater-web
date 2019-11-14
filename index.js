5fcab01784dcd7ea0db32cdb16be7faee28a398f
Jonathan Ray <jonathanray17@gmail.com>
2019-11-14 09:01:09 -0500

    adds ability to edit message

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-app.use('*', (req, res) => {
-	res.json({ msg: 'success' });
+let text = { msg: 'ho ho ho ' };
+app.post('*', (req, res) => {
+	text.msg = req.query.msg || 'ho ho ho ';
+	res.json(text);
+});
+app.get('*', (req, res) => {
+	res.json(text);
 });
 app.listen(3030, () => console.log('running'));
