bd7413ab9362c5e22c5897f9bc6f550cf009f7e5
Jonathan Ray <jonathanray17@gmail.com>
2019-11-19 12:06:36 -0500

    adds files for work on UpsideDown

---
+++ react/src/components/UpsideDown.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Input from './Input';
+import './UpsideDown.css';
+function UpsideDown(props) {
+	return (
+		<div className='upside-down'>
+			<Input />
+		</div>
+	);
+}
+export default UpsideDown;
