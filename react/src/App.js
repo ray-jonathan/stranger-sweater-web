36353645ab633c0f1333ada5bfa73da0df3b5408
Jonathan Ray <jonathanray17@gmail.com>
2019-11-19 11:43:17 -0500

    dynamically adjusts background height to be a perfect fit

--- react/src/App.js
+++ react/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import Input from './components/Input';
 import Title from './components/Title';
 import SweaterVest from './components/SweaterVest';
+import UpsideDown from './components/UpsideDown';
 
 function App() {
 	return (
 		<div className='app'>
-			<Title />
-			<SweaterVest />
-			<Input />
+			<div className='sweater-background'>
+				<Title />
+				<SweaterVest />
+			</div>
+			<UpsideDown />
 		</div>
 	);
 }
